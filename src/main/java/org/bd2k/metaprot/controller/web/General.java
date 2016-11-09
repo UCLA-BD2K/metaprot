dcc726f3ac2c2d26cb35d0705665bcef32815c60
Allen Gong <alm.gong@gmail.com>
2016-11-08 16:42:55 -0800

    overhaul UI to accommadate new site structure

--- src/main/java/org/bd2k/metaprot/controller/web/General.java
+++ src/main/java/org/bd2k/metaprot/controller/web/General.java
@@ -1,6 +1,8 @@
 package org.bd2k.metaprot.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,18 +15,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class General {
 
+    /**
+     * Home page
+     * @return
+     */
     @RequestMapping("/")
     public String getHomePage() {
         return "index";
     }
 
+    /**
+     * Invoked when user wants to upload a file for analysis.
+     * @return
+     */
     @RequestMapping("/upload")
     public String getUploadPage() {
         return "upload";
     }
 
-    @RequestMapping("/upload-pass")
-    public String getUploadPassPage() {
+    /**
+     * Invoked when an upload + integrity check was successful, and a
+     * corresponding view page needs to be returned for the next steps.
+     * @param token
+     * @return
+     */
+    @RequestMapping("/upload-pass/{token}")
+    public String getUploadPassPage(Model model, @PathVariable("token") String token) {
+        System.out.println(token);
+
+        model.addAttribute("token", token);     // pass token to view as model variable
+
+
         return "upload_pass";
     }
 
